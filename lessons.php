93716297a26cf41b0a05ad0b98198d61b36ffdad
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-11-08 20:47:04 +0800

    add lessons.php

---
+++ lessons.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>intro to php</title>
+</head>
+<body>
+    <h1>intro to PhP</h1>
+    <h2>10 primitive types</h2>
+    <h2>Scalar</h2>
+    <ul>
+        <li>string</li>
+        <li>integer</li>
+        <li>float</li>
+        <li>boolean</li>
+    </ul>
+
+    <h2>compound</h2>
+    <ul>
+        <li>array</li>
+        <li>objects</li>
+        <li>callable</li>
+        <li>iterable</li>
+    </ul>
+
+    <h2>Special</h2>
+    <ul>
+        <li>resource</li>
+        <li>null</li>
+    </ul>
+    <h2>Pseudo types</h2>
+    <hr/>
+</body>
+</html>
