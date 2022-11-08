5029abc290fb783c8a14b13af8446e5961fa7d07
rebeccaacero1 <rebeccaacero1@gmail.com>
2022-11-08 20:03:02 +0800

    first PHP

---
+++ myfirstPhp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first php file</title>
+</head>
+<body>
+    <h1>My First PhP</h1>
+    <?php
+        echo "my first php script"
+    ?>
+</body>
+</html>
